638d9f45564733f001a3bbec091c98dd1430e149
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-28 08:43:31 +0000

    Add more items to snowmanitem

--- Snowy/SwiftData/SnowmanItem.swift
+++ Snowy/SwiftData/SnowmanItem.swift
@@ -11,15 +11,55 @@ import SwiftData
 @Model class SnowmanItem {
     var snowmanName: String
     var creationDate: Date
+    
+    var ball0Size: Double
+    var ball1Size: Double
+    var ball2Size: Double
+    
+    var buttonCount: Int
+    var buttonStyle: Int // 0 circle, 1 square
+    
+    var showingHat: Bool
+    var hatColourIndex: Int
+    
+    var showingNose: Bool
+    var noseStyleIndex: Int // 0 circle, 1 square
+    var noseColourIndex: Int
+    
     var id = UUID()
     
     init(
         snowmanName: String,
         creationDate: Date,
+        ball0Size: Double,
+        ball1Size: Double,
+        ball2Size: Double,
+        buttonCount: Int,
+        buttonStyle: Int,
+        showingHat: Bool,
+        hatColourIndex: Int,
+        showingNose: Bool,
+        noseStyleIndex: Int,
+        noseColourIndex: Int,
         id: UUID = UUID()
-    ){
+    ) {
         self.snowmanName = snowmanName
         self.creationDate = creationDate
+        
+        self.ball0Size = ball0Size
+        self.ball1Size = ball1Size
+        self.ball2Size = ball2Size
+        
+        self.buttonCount = buttonCount
+        self.buttonStyle = buttonStyle
+        
+        self.showingHat = showingHat
+        self.hatColourIndex = hatColourIndex
+        
+        self.showingNose = showingNose
+        self.noseStyleIndex = noseStyleIndex
+        self.noseColourIndex = noseColourIndex
+        
         self.id = id
     }
 }
